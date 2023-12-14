8ea59dfcb1b9d14f06b033951c6bed427d1eee58
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-14 05:34:20 +0300

    define the table constraints

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean,UniqueConstraint
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean,UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
@@ -8,6 +8,10 @@ Base= declarative_base()
 class LectureHall(Base):
     __tablename__='lecture_halls'
 
+    __table_args__=(
+        UniqueConstraint('name', name='uq_lecture_hall_name')
+        PrimaryKeyConstraint('id', name='pk_lecture_hall_id')
+    )
 
     id = Column(Integer(), primary_key=True)
     name= Column(String(), unique=True, nullable= False)
