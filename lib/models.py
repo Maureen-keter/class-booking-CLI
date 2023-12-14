2f0bdb7ec912d8e09ebf9c3186737bc8166e86c2
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-14 06:07:58 +0300

    define the table columns

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean,UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from datetime import DateTime
 
 Base= declarative_base()
 
 class School(Base):
-    __table__name='schools'
-    
+    __tablename__='schools'
+    id=Column(Integer(), primary_key=True)
+    name=Column(String(), nullable=False)
+    lecture_halls=relationship('LectureHall',back_populates='school')
+
 
 class LectureHall(Base):
     __tablename__='lecture_halls'
