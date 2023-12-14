d92e2916a169977e3d7525fc6ff484dca795bf96
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-14 06:03:58 +0300

    define a class school

--- lib/models.py
+++ lib/models.py
@@ -1,16 +1,16 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean,UniqueConstraint, PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+from datetime import DateTime
 
 Base= declarative_base()
 
+class School(Base):
+    __table__name='schools'
+    
+
 class LectureHall(Base):
     __tablename__='lecture_halls'
-
-    __table_args__=(
-        UniqueConstraint('name', name='uq_lecture_hall_name'),
-        PrimaryKeyConstraint('id', name='pk_lecture_hall_id'))
 
     id = Column(Integer(), primary_key=True)
     name= Column(String(), unique=True, nullable= False)
