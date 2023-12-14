8346d83d557f5397967218f979090efbf03cf6a7
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-14 05:32:14 +0300

    define the table columns

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,17 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean,UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-Base= declarative_base
+Base= declarative_base()
+
+class LectureHall(Base):
+    __tablename__='lecture_halls'
+
+
+    id = Column(Integer(), primary_key=True)
+    name= Column(String(), unique=True, nullable= False)
+    status=Column(Boolean(), default= False)
+    last_used=Column(DateTime())
+
+
