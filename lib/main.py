3a2a35a5c6d920c426dda4a4a18acfc22eba13fb
maureen <maureen.chelangat@student.moringaschool.com>
2023-12-15 12:48:19 +0300

    define a function that adds lecture halls to a specific school and return error if school is not found

--- lib/main.py
+++ lib/main.py
@@ -8,3 +8,24 @@ def add_school():
     session.commit()
     click.echo(f'School "{school.name}" added successfully')
 
+def add_lecture_hall():
+    school_name = click.prompt('Enter school name', type=str)
+    school = session.query(School).filter_by(name=school_name).first()
+
+    if not school:
+        click.echo(f'School "{school_name}" not found.')
+        return
+
+    lecture_hall_name = click.prompt('Enter lecture hall name', type=str)
+    lecture_hall = LectureHall(name=lecture_hall_name, school=school)
+    session.add(lecture_hall)
+    session.commit()
+    click.echo(f'Lecture hall "{lecture_hall.name}" added successfully')
+
+
+
+
+
+
+
+
